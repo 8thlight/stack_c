b0a40740c816bba7d8fd639744284ff86b061bbc
dougbradbury <doug@8thlight.com>
2014-10-28 20:50:15 -0500

    starting point

--- src/Stack.c
+++ src/Stack.c
@@ -4,41 +4,18 @@
 
 struct Stack
 {
-  int count;
-  int * data;
+  int placeholder;
 };
 
 Stack* Stack_Create(int capacity)
 {
   Stack* self = malloc(sizeof(Stack));
   memset(self, 0, sizeof(Stack));
-  self->data = calloc(capacity, sizeof(int));
   return self;
 }
 
 void Stack_Destroy(Stack* self)
 {
-  free(self->data);
   free(self);
 }
 
-int Stack_isEmpty(Stack* self)
-{
-  return self->count == 0;
-}
-
-void Stack_push(Stack* self, int i)
-{
-  self->data[self->count] = i;
-  self->count++;
-}
-
-void Stack_pop(Stack* self)
-{
-  self->count--;
-}
-
-int Stack_top(Stack* self)
-{
-  return self->data[self->count - 1];
-}
--- test/test_Stack.c
+++ test/test_Stack.c
@@ -18,44 +18,44 @@ void test_stack_create(void) {
 //UnComment as you go, revealing one test at a time.
 //Building the Stack as you go
 
-void test_empty(void) {
-  TEST_ASSERT_TRUE(Stack_isEmpty(stack));
-}
-
-void test_not_empty(void) {
-  Stack_push(stack, 1);
-  TEST_ASSERT_FALSE(Stack_isEmpty(stack));
-}
-
-void test_pop_empty(void) {
-  Stack_push(stack, 1);
-  Stack_pop(stack);
-  TEST_ASSERT_TRUE(Stack_isEmpty(stack));
-}
-
-void test_push_pop_many(void) {
-  Stack_push(stack, 1);
-  Stack_push(stack, 1);
-  Stack_pop(stack);
-  TEST_ASSERT_TRUE(!Stack_isEmpty(stack));
-}
-
-void test_push_value(void) {
-  Stack_push(stack, 21);
-  TEST_ASSERT_EQUAL(21, Stack_top(stack));
-}
-
-void test_push_multiple_values(void) {
-  Stack_push(stack, 21);
-  Stack_push(stack, 28);
-  Stack_push(stack, 12);
-  TEST_ASSERT_EQUAL(12, Stack_top(stack));
-  Stack_pop(stack);
-  TEST_ASSERT_EQUAL(28, Stack_top(stack));
-  Stack_pop(stack);
-  TEST_ASSERT_EQUAL(21, Stack_top(stack));
-  Stack_pop(stack);
-}
+//void test_empty(void) {
+//  TEST_ASSERT_TRUE(Stack_isEmpty(stack));
+//}
+//
+//void test_not_empty(void) {
+//  Stack_push(stack, 1);
+//  TEST_ASSERT_FALSE(Stack_isEmpty(stack));
+//}
+//
+//void test_pop_empty(void) {
+//  Stack_push(stack, 1);
+//  Stack_pop(stack);
+//  TEST_ASSERT_TRUE(Stack_isEmpty(stack));
+//}
+//
+//void test_push_pop_many(void) {
+//  Stack_push(stack, 1);
+//  Stack_push(stack, 1);
+//  Stack_pop(stack);
+//  TEST_ASSERT_TRUE(!Stack_isEmpty(stack));
+//}
+//
+//void test_push_value(void) {
+//  Stack_push(stack, 21);
+//  TEST_ASSERT_EQUAL(21, Stack_top(stack));
+//}
+//
+//void test_push_multiple_values(void) {
+//  Stack_push(stack, 21);
+//  Stack_push(stack, 28);
+//  Stack_push(stack, 12);
+//  TEST_ASSERT_EQUAL(12, Stack_top(stack));
+//  Stack_pop(stack);
+//  TEST_ASSERT_EQUAL(28, Stack_top(stack));
+//  Stack_pop(stack);
+//  TEST_ASSERT_EQUAL(21, Stack_top(stack));
+//  Stack_pop(stack);
+//}
 
 
 // What's next? ...
